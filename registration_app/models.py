fef79a1691f5e35819e116ed4b27307e2bc03981
Ebin Paul <ebinpaul@outlook.com>
2020-08-18 23:44:05 +0530

    registration DB config

--- registration_app/models.py
+++ registration_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class RegisterUser(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
