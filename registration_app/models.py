b2a3b90df4559fa2d6ecbb8718e750c15ef4d20c
Ebin Paul <ebinpaul@outlook.com>
2021-04-13 09:07:15 +0530

    Update models.py

--- registration_app/models.py
+++ registration_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class RegisterUser(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
