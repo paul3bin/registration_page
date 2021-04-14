99a786ebf7d43d29a49b71a82833bbf428e27ada
Ebin Paul <ebinpaul@outlook.com>
2021-04-14 22:09:59 +0530

    Update views.py

--- registration_app/views.py
+++ registration_app/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render
-from registration_app import forms
-from registration_app import models
+from . import forms, models
 # Create your views here.
+
 
 def index(request):
     if request.method == 'POST':
         return registration(request)
-    return render(request,'registration/index.html')
+    return render(request, 'registration/index.html')
+
 
 def registration(request):
     form = forms.UserRegisterForm()
-    if request.method== 'POST':
+    if request.method == 'POST':
         form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
             return thanks(request)
         else:
             print('Form invalid!')
-    return render(request,'registration/registration.html', context={'formkey': form})
+    return render(request, 'registration/registration.html', context={'formkey': form})
+
 
 def thanks(request):
-    if request.method== 'POST':
+    if request.method == 'POST':
         return index(request)
-    return render(request,'registration/thanks.html')
+    return render(request, 'registration/thanks.html')
