0c3dd423038f208014ef378be02d643be95de25f
Ebin Paul <ebinpaul@outlook.com>
2021-04-14 22:09:48 +0530

    Update admin.py

--- registration_app/admin.py
+++ registration_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from registration_app.models import RegisterUser
+from .models import RegisterUser
 # Register your models here.
 
 admin.site.register(RegisterUser)
